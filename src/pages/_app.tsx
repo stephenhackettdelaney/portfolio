edf33136b18d852c06a61c1922c8499498dba782
stephenhackettdelaney <stephenhackettdelaney@gmail.com>
2024-08-28 16:15:04 -0700

    test nextjs googleTagManager implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -9,6 +10,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
+            <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTMI!} />
             <Script
                 strategy="lazyOnload"
                 src="https://www.stephenhd.com/script.js"
